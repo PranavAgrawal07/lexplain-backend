48e725d26f4083110cac25ae357250c9e8241028
PranavAgrawal07 <agrawalpranav33@gmail.com>
2026-06-03 11:09:14 +0530

    Fix CORS: add explicit headers middleware before routes

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const cors = require("cors");
 const Groq = require("groq-sdk");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
